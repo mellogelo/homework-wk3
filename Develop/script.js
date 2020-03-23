972f5d19922fe76ba7077e845ada36d3e801ac14
Ralph Magbanua <rmagbanua09@gmail.com>
2020-03-22 21:42:28 -0700

    added criteria to function and variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Variables for Criteria
+var lowercase;
+var uppercase;
+var characters;
+var numbers;
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -15,7 +21,17 @@ function writePassword(event) {
   event.preventDefault();
 //prompts user how many characters unsing only digits
   var passlength = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
-  console.log(passlength);
+  if (!passlength) {
+    alert('This needs a value');
+  } else if (passlength < 8 || passlength > 128) {
+    passlength = parseInt(prompt("You must choose between 8 and 128"));
+  } else {
+    lowercase = confirm("Will this contain lowercase?")
+    uppercase = confirm("Will this contain uppercase?")
+    characters = confirm("Will this contain character?")
+    numbers = confirm("Will this contain numbers?")
+  };
+
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
